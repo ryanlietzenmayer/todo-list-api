d86a9e8906ba9034171e93db04bf683aa55582a2
Ryan Lietzenmayer <ryan.lietz@gmail.com>
2026-06-04 13:45:01 +0800

    move model

--- Program.cs
+++ Program.cs
@@ -1,15 +1,5 @@
 ﻿using System.Text.Json; // Required for JSON
 
-public class ToDoItem
-{
-    public int id { get; set; }
-    public string taskDescription { get; set; }      
-    public DateTime createdDate { get; set; }
-    public DateTime dueDate { get; set; }
-    public bool completed { get; set; }  
-}
-
-
 class Program
 {
     // runs the app
