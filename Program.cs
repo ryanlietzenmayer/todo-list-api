0e83639b1f7897b430c31aa68e71a5ffe5571481
Ryan Lietzenmayer <ryan.lietz@gmail.com>
2026-06-04 12:26:39 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Text.Json; // Required for JSON
+
+public class ToDoItem
+{
+    public string Title { get; set; }      
+    public bool IsCompleted { get; set; }  
+}
+
+
+class Program
+{
+    // runs the app
+    static void Main(string[] args)
+    {
+    }
+}
